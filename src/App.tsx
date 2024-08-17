fa71a63f7c4502b5c359fde454c91845934b86db
Srivastava57Harsh <srivastava57harsh@gmail.com>
2024-08-17 12:01:19 +0530

    :bug: Fix loader bug
    fix loader bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import SearchBar from "./components/Searchbar/searchbar";
 import Hero from "./components/Hero/hero";
@@ -6,36 +6,17 @@ import Ribbon from "./components/Ribbon/ribbon";
 import Pillars from "./components/Pillars/pillars";
 import "./App.css";
 import MobileCarousel from "./components/Carousels/mobilecarousels";
-import Loader from "./components/Loader/loader";
 
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      setIsLoading(false);
-    };
-
-    window.addEventListener("load", handleLoad);
-
-    return () => window.removeEventListener("load", handleLoad);
-  }, []);
-
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className="app">
-          <Navbar />
-          <Hero />
-          <SearchBar />
-          <Ribbon />
-          <MobileCarousel />
-          <Pillars />
-        </div>
-      )}
-    </>
+    <div className="app">
+      <Navbar />
+      <Hero />
+      <SearchBar />
+      <Ribbon />
+      <MobileCarousel />
+      <Pillars />
+    </div>
   );
 };
 
